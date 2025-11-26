fbdf6cf8e917e23189d0a1f2ddf6d9fb9ff5fd02
Pedro Henrique <p13dr0h@gmail.com>
2025-11-26 18:05:11 -0300

    feat: implement use case to list genres with query

--- application/src/main/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/GenreListOutput.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/GenreListOutput.java
@@ -1,4 +1,29 @@
 package com.fullcycle.admin.catalogo.application.genre.retrieve.list;
 
-public record GenreListOutput() {
+import com.fullcycle.admin.catalogo.domain.genre.Genre;
+
+import java.time.Instant;
+import java.util.List;
+
+public record GenreListOutput(
+        String id,
+        String name,
+        boolean isActive,
+        List<String> categories,
+        Instant createdAt,
+        Instant deletedAt
+) {
+
+    public static GenreListOutput from(final Genre aGenre) {
+        return new GenreListOutput(
+                aGenre.getId().getValue(),
+                aGenre.getName(),
+                aGenre.isActive(),
+                aGenre.getCategories().stream()
+                        .map(categoryID -> categoryID.getValue())
+                        .toList(),
+                aGenre.getCreatedAt(),
+                aGenre.getDeletedAt()
+        );
+    }
 }
--- application/src/test/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/ListGenreUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/genre/retrieve/list/ListGenreUseCaseTest.java
@@ -1,15 +1,120 @@
 package com.fullcycle.admin.catalogo.application.genre.retrieve.list;
 
 import com.fullcycle.admin.catalogo.application.UseCaseTest;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.genre.Genre;
+import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class ListGenreUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultListGenreUseCase useCase;
+
+    @Mock
+    private GenreGateway gateway;
+
     @Override
     protected List<Object> getMocks() {
-        return List.of();
+        return List.of(gateway);
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenres_thenShouldReturnGenres() {
+        final var genres = List.of(
+                Genre.newGenre("Ação", true),
+                Genre.newGenre("Comédia", true)
+        );
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var expectedPagination =
+                new Pagination<>(expectedPage, expectedPerPage, genres.size(), genres);
+
+        final var expectedItemsCount = 2;
+        final var expectedResult = expectedPagination.map(GenreListOutput::from);
+
+        when(gateway.findAll(eq(aQuery)))
+                .thenReturn(expectedPagination);
+
+        final var actualResult = useCase.execute(aQuery);
+
+        Assertions.assertEquals(expectedItemsCount, actualResult.items().size());
+        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedPage, actualResult.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualResult.perPage());
+        Assertions.assertEquals(genres.size(), actualResult.total());
+    }
+
+    @Test
+    public void givenAValidQuery_whenHasNoResults_thenShouldReturnEmptyGenres() {
+        final var genres = List.<Genre>of();
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var expectedPagination =
+                new Pagination<>(expectedPage, expectedPerPage, genres.size(), genres);
+
+        final var expectedItemsCount = 0;
+        final var expectedResult = expectedPagination.map(GenreListOutput::from);
+
+        when(gateway.findAll(eq(aQuery)))
+                .thenReturn(expectedPagination);
+
+        final var actualResult = useCase.execute(aQuery);
+
+        Assertions.assertEquals(expectedItemsCount, actualResult.items().size());
+        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(expectedPage, actualResult.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualResult.perPage());
+        Assertions.assertEquals(genres.size(), actualResult.total());
+    }
+
+    @Test
+    public void givenAValidQuery_whenGatewayThrowsException_shouldReturnException() {
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        when(gateway.findAll(eq(aQuery)))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException =
+                Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aQuery));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
     }
 }
